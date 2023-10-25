1f27bbc1677c7349a09e2466abb418679312004e
sstadelmannsiw <simon.stadelmann@lernen.siw.swiss>
2023-10-25 20:30:15 +0200

    Definieren von neuen Variablen

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CategoryModalComponent } from '../category-modal/category-modal.component';
 import { ModalController } from '@ionic/angular';
-import { Category } from '../../shared/domain';
+import {Category, CategoryCriteria} from '../../shared/domain';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
 })
 export class CategoryListComponent {
+  categories: Category[] | null = null;
+  readonly initialSort = 'name,asc';
+  lastPageReached = false;
+  loading = false;
+  searchCriteria: CategoryCriteria = { page: 0, size: 25, sort: this.initialSort };
   constructor(private readonly modalCtrl: ModalController) {}
 
   async openModal(category?: Category): Promise<void> {
