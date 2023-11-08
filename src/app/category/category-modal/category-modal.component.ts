afdd355aabb24f131e984524cf3c0ad8b9682a86
sstadelmannsiw <simon.stadelmann@lernen.siw.swiss>
2023-11-08 21:59:28 +0100

    Delete Methode ersetzen

--- src/app/category/category-modal/category-modal.component.ts
+++ src/app/category/category-modal/category-modal.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ActionSheetService } from '../../shared/service/action-sheet.service';
-import {filter, finalize, from} from 'rxjs';
+import {filter, finalize, from, mergeMap, tap} from 'rxjs';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {CategoryService} from "../category.service";
 import {ToastService} from "../../shared/service/toast.service";
@@ -52,11 +52,18 @@ export class CategoryModalComponent {
 
   delete(): void {
     from(this.actionSheetService.showDeletionConfirmation('Are you sure you want to delete this category?'))
-      .pipe(filter((action) => action === 'delete'))
+      .pipe(
+        filter((action) => action === 'delete'),
+        tap(() => (this.submitting = true)),
+        mergeMap(() => this.categoryService.deleteCategory(this.category.id!)),
+        finalize(() => (this.submitting = false)),
+      )
       .subscribe({
         next: () => {
-          this.modalCtrl.dismiss(null, 'delete');
+          this.toastService.displaySuccessToast('Category deleted');
+          this.modalCtrl.dismiss(null, 'refresh');
         },
+        error: (error) => this.toastService.displayErrorToast('Could not delete category', error),
       });
   }
 }
